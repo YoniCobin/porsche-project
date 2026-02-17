cbb6d42b418c1bb5f7c42af45d8b5a8ddd47da21
Yehonatan Cobin <yonicobin@gmail.com>
2026-02-17 12:06:39 +0200

    Initial commit

---
+++ src/main/java/com/example/porsche_project/PorscheProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.porsche_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PorscheProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PorscheProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/porsche_project/PorscheProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.porsche_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PorscheProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
